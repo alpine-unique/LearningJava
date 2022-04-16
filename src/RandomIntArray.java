2caa2729187c43cb1ff3fe031dd18c4264beebac
alpine-unique <willbemorecautious@gmail.com>
2022-04-17 00:16:37 +0300

    task 4

--- src/RandomIntArray.java
+++ src/RandomIntArray.java
@@ -9,32 +9,27 @@ public class RandomIntArray {
     public static void filler() {
         Random randomarray = new Random();
         int[] array = randomarray.ints(20, -10, 20).toArray();
-        maxNegative(array);
-        minPositive(array);
-
+        toListAndSwap(array, minPositive(array), maxNegative(array));
     }
-    public static void maxNegative(int[] array) {
+    public static int maxNegative(int[] array) {
         int maxnegative = -1;
         for (int i = 0; i < array.length; i++) {
             if(array[i] < 0 && (maxnegative == -1 || array[i] > array[maxnegative])) {
                 maxnegative = i;
             }
         }
-        System.out.println(Arrays.toString(array));
-        System.out.println("А оно надо? " + array[maxnegative]);
-        System.out.println("Индекс максимального отриц. элемента: " + maxnegative);
+       return maxnegative;
     }
-    public static void minPositive(int[] array) {
+    public static int minPositive(int[] array) {
         int minpositive = 1;
         for (int i = 0; i <array.length; i++) {
             if(array[i] > 0 && (minpositive == 1 || array[i] < array[minpositive])) {
                 minpositive = i;
             }
         }
-        System.out.println(array[minpositive]);
-        System.out.println("Индекс минимального положит. элемента: " + minpositive);
+        return minpositive;
     }
-    public static void toListAndSwap (int[] array,int minpositive,int maxnegative) {
+    public static void toListAndSwap (int[] array, int minpositive, int maxnegative) {
         List<Integer> list = new ArrayList<Integer>(array.length);
         for (int i: array) {
             list.add(i);
